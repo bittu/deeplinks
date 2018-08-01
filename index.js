a4d538b17b9a155882ace66fcda4213e8f03817c
Sandeep Vemula <sandeepvemula@Lakshmans-MacBook-Pro.local>
2018-08-01 13:59:48 +0100

    aasa updated with bttv beta

--- index.js
+++ index.js
@@ -46,11 +46,11 @@ app.get('/.well-known/assetlinks.json', (req, res) => {
 /* ios */
 app.get('/apple-app-site-association', (req, res) => {
 	res.setHeader('Content-Type', 'application/json');
-	res.send('{"applinks": {"apps": [],"details": [{"appID": "<YOUR_TEAM_ID_FROM_MEMBER_CENTER>.com.test.bt","paths": ["/openapp.html"]}]}}');
+	res.send('{"applinks": {"apps": [],"details": [{"appID": "RQC7JGZ7X5.com-bt-tve-beta","paths": ["/openapp.html"]}]}}');
 })
 app.get('/.well-known/apple-app-site-association', (req, res) => {
 	res.setHeader('Content-Type', 'application/json');
-	res.send('{"applinks": {"apps": [],"details": [{"appID": "<YOUR_TEAM_ID_FROM_MEMBER_CENTER>.com.test.bt","paths": ["/openapp.html"]}]}}');
+	res.send('{"applinks": {"apps": [],"details": [{"appID": "RQC7JGZ7X5.com-bt-tve-beta","paths": ["/openapp.html"]}]}}');
 })
 
 app.get('/openapp.html', (req, res) => {
